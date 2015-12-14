7afa33c4c6a90184592b9103a55f849acc2fca94
Ty <tytowry@gmail.com>
2015-12-14 02:13:34 -0800

    update

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+
+	// the database url to connect
+	url : 'mongodb://http://apollo.modulusmongo.net:27017/weQ9axyp'
---
+++ public/js/services/todos.js
@@ -0,0 +1,15 @@
+angular.module('todoService', [])
+
+	.factory('Todos', ['$http',function($http) {
+		return {
+			get : function() {
+				return $http.get('/api/todos');
+			},
+			create : function(todoData) {
+				return $http.post('/api/todos', todoData);
+			},
+			delete : function(id) {
+				return $http.delete('/api/todos/' + id);
+			}
+		}
+	}]);
